ffd910ade97c0a31e8fccad90c73c190ff1d73f5
wataken44 <wataken44@hotmail.com>
2023-10-28 03:49:43 +0000

    update to python3.11

--- httputil/main.py
+++ httputil/main.py
@@ -6,14 +6,14 @@ IP(REMOTE_ADDR) viewer on Google App Engine
 
 """
 
-import webapp2
-import os
+from flask import Flask, make_response, request
 
-class IndexPage(webapp2.RequestHandler):
-    def get(self):
-        host = os.environ['HTTP_HOST']
-        self.response.out.write("""
-<!DOCTYPE html>
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    host = request.environ['HTTP_HOST']
+    return """<!DOCTYPE html>
 <html>
 <body>
 <a href="http://%s/ip">ip</a><br />
@@ -21,25 +21,18 @@ class IndexPage(webapp2.RequestHandler):
 <a href="http://twitter.com/wataken44">@wataken44</a>
 </body>
 </html>
-""" % host)
+""" % host 
 
-    def head(self):
-        pass
 
-class IPPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.headers['Access-Control-Allow-Origin'] = '*'
-        addr = os.environ['REMOTE_ADDR']
-        self.response.out.write(addr)
+@app.route("/ip")
+def ip():
+    addr = request.environ['REMOTE_ADDR']
+    resp = make_response(addr)
 
-    def head(self):
-        pass
-
-application = webapp2.WSGIApplication(
-    [('/', IndexPage),
-     ('/ip', IPPage)]
-    )
+    resp.headers['Content-Type'] = 'text/plain'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    
+    return resp
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=8080, debug=True)
