112af1e61cb4e871a1056aa7f761200ea5921e94
wataken44 <wataken44@hotmail.com>
2023-10-28 04:31:01 +0000

    add env

--- httputil/main.py
+++ httputil/main.py
@@ -26,12 +26,38 @@ def index():
 
 @app.route("/ip")
 def ip():
-    addr = request.environ['REMOTE_ADDR']
+    addr = request.headers.get('X-Appengine-User-IP')
+    if addr is None:
+        addr = request.environ.get("REMOTE_ADDR", "")
     resp = make_response(addr)
 
     resp.headers['Content-Type'] = 'text/plain'
     resp.headers['Access-Control-Allow-Origin'] = '*'
     
+    return resp
+
+@app.route("/env")
+def env():
+    keys = [
+        'X-Appengine-Api-Ticket',
+        'X-Appengine-City',
+        'X-Appengine-CityLatLong',
+        'X-Appengine-Country',
+        'X-Appengine-Default-Version-Hostname',
+        'X-Appengine-Https',
+        'X-Appengine-Region',
+        'X-Appengine-Request-Log-Id',
+        'X-Appengine-Timeout-Ms',
+        'X-Appengine-User-IP',
+        'X-Cloud-Trace-Context',
+        'X-Forwarded-For',
+        'X-Forwarded-Proto',
+    ]
+    
+    resp = {}
+    for k in keys:
+        resp[k] = request.headers.get(k)
+
     return resp
 
 if __name__ == "__main__":
